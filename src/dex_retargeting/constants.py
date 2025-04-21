5542609a9b5befd7e0d4d8cae9502f21b68d925f
rpwang <wangrenpeng97@gmail.com>
2025-04-21 15:49:41 +0800

    add botyard hand map

--- src/dex_retargeting/constants.py
+++ src/dex_retargeting/constants.py
@@ -29,6 +29,7 @@ class RobotName(enum.Enum):
     ability = enum.auto()
     inspire = enum.auto()
     panda = enum.auto()
+    botyard = enum.auto()
 
 
 class RetargetingType(enum.Enum):
@@ -52,6 +53,7 @@ ROBOT_NAME_MAP = {
     RobotName.ability: "ability_hand",
     RobotName.inspire: "inspire_hand",
     RobotName.panda: "panda_gripper",
+    RobotName.botyard: "botyard_hand",
 }
 
 ROBOT_NAMES = list(ROBOT_NAME_MAP.keys())
